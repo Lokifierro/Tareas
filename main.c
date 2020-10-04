151bdb6fc94dfcd585f2cb67569d9de04aaeb401
Lokifierro <71135457+Lokifierro@users.noreply.github.com>
2020-10-04 15:58:34 -0500

    Add files via upload

--- main.c
+++ main.c
@@ -1,35 +1,29 @@
-//Programa para calcular resistencias de LED's
+//Programa para calcular corrientes y voltajes en un circuito en paralelo
 //27/09/2020
 //Realizado por Humberto Sanluis Cervantes
 
 #include <stdio.h>
 int main()
 {
-     float Vs; //voltaje de la fuente
-     int R1,R2,R3;
-     float VF1,VF2,VF3; //voltaje de los LED's
-     float IF1,IF2,IF3;// amperaje de los LED's
-
-     //LED rojo brillante
-     VF1=2;
-     IF1=0.02;
-     //LED azul brillante
-     VF2=3;
-     IF2=0.02;
-     //LED blanco
-     VF3=2.8;
-     IF3=0.02;
+     float Vt; //voltaje de la fuente
+     int R1,R2,R3;//Resistencias
+     float I1,I2,I3;// corrientes
 
      printf("Ingrese el valor de la fuente de voltaje:\n");
-     scanf("%f", &Vs);
+     scanf("%f",&Vt);
+     printf("ingrese los valores de las resistencias separadas por comas:\n");
+     scanf("%d,%d,%d", &R1, &R2, &R3);
 
-     R1=(Vs-VF1)/IF1;
-     R2=(Vs-VF2)/IF2;
-     R3=(Vs-VF3)/IF3;
+     I1=Vt/R1;
+     I2=Vt/R2;
+     I3=Vt/R3;
 
-     printf("La resistencia para el LED rojo brillante es \n%d Ohms", R1);
-     printf("\nLa resistencia para el LED azul brillante es \n%d Ohms", R2);
-     printf("\nLa resistencia para el LED blanco es \n%d Ohms", R3);
+     printf("La corriente que pasa por la resistencia 1 es %.2f\n", I1);
+     printf("La corriente que pasa por la resistencia 2 es %.2f\n", I2);
+     printf("La corriente que pasa por la resistencia 3 es %.2f\n", I3);
+     printf("El voltaje que pasa por cada resistencia es el mismo del voltaje de la fuente");
+
+
 
 
     return 0;
